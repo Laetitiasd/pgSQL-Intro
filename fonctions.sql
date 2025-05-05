cc231ca0efc04018c8abee1ad64da8f72f049273
Laetitiasd <144696523+Laetitiasd@users.noreply.github.com>
2025-05-05 09:03:10 +0200

    Update fonctions.sql

--- fonctions.sql
+++ fonctions.sql
@@ -171,3 +171,109 @@ $$ LANGUAGE plpgsql;
 
 
 
+
+/*Exercice 6*/
+
+create or replace function getNbClientsDebiteur() returns integer as
+$$
+
+	declare
+	
+		i integer;
+		ii integer;
+		max integer;
+	
+	begin
+		
+		i := 0;
+		ii := 0;
+		select max(id_operation) into max from operation;
+		while i < max
+			loop
+				if exists (select num_compte from operation where type_operation = 'DEBIT' and num_compte = i) then
+					ii := ii + 1;
+				end if;
+				i := i + 1;
+			end loop;
+			
+	return ii;		
+	
+	end;
+
+$$
+language plpgsql;
+
+/*Exercice 7*/
+
+create or replace function nb_client_ville(text) returns integer as
+$$
+
+	declare
+		ville text;
+		split text;
+		i integer;
+		ii integer;
+		iii integer;
+		max integer;
+		compteur integer;
+		
+	
+	begin
+		i := 1;
+		select max(num_client) into max from client;
+		while i < max
+			loop
+				if exists (select adresse_client from client where num_client = i) then 
+					select adresse_client into ville from client where num_client = i;
+					split := split_part(ville, ', ', 2);
+					
+					ii := position(' ' in split);
+					iii := char_length(split);
+					split := substr(split, ii + 1, iii);
+					
+					if lower($1) = lower(split)then
+						select count(num_client) into compteur from client where adresse_client = ville;
+						return compteur;
+					end if;
+					else
+						compteur := 0;
+					
+				end if;
+			i := i + 1;
+			end loop;
+		return compteur;
+						
+			
+			
+	
+	end;
+
+$$
+language plpgsql;
+
+/*Exercice 8*/
+create or replace function enregistrer_client(text, text, text, text, text) returns text as
+$$
+	declare
+	
+		i integer;
+		max integer;
+		max2 integer;
+
+	begin
+		
+		select max(num_client) into max from client;
+		max := max + 1;
+		execute 'insert into client values('||max||' , '''||$1||''' , '''||$2||''' , '''||$3||''' , '''||$4||''' , '''||$5||''')' ;
+		select max(num_client) into max2 from client;
+		if exists(select * from client where num_client = max and nom_client = $1 and prenom_client = $2)then
+			
+			return '1';
+		end if;
+		return '0';	
+		
+	end;
+	
+$$
+language plpgsql;	
+
